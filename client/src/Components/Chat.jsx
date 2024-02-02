90948f5f705dcece814da077a71484fdffd4c456
MrAIRobot <gfhieemobilelegends@gmail.com>
2024-02-02 17:38:57 +0300

    chat design

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -8,35 +8,57 @@ const Chat = ({socket, username, msgArray}) => {
     const params = useParams();
 
     useEffect(() => {
-        console.log(msgArray);
         let chatContainer = document.getElementById('idchat');
         chatContainer.innerHTML = '';
         msgArray.forEach(function (message) {
-            console.log(message);
             let messageElement = document.createElement('div');
             message.user === username ? messageElement.className = "msg author" : messageElement.className = "msg"
-            messageElement.textContent = message.user + ': ' + message.text;
             chatContainer.appendChild(messageElement);
+
+            let messageHeader = document.createElement('div');
+            messageHeader.className = "msg_header";
+            messageElement.appendChild(messageHeader);
+
+            let authorSpan = document.createElement('span');
+            authorSpan.className = "msg_author";
+            authorSpan.textContent = message.user;
+            messageHeader.appendChild(authorSpan);
+
+            let timeSpan = document.createElement('span');
+            timeSpan.className = "msg_time";
+            timeSpan.textContent = "12:01";
+            messageHeader.appendChild(timeSpan);
+
+            let messageText = document.createElement('div');
+            messageText.className = "msg_text";
+            messageText.textContent =  message.text;
+            messageElement.appendChild(messageText);
         });
 
         chatContainer.scrollTop = chatContainer.scrollHeight;
     }, [msgArray]);
 
+    const sendHandler = () => {
+        // let tmp = inputRef.current.value;
+
+        socket.current.send(JSON.stringify({
+            id: params.id,
+            username: username,
+            method: 'message',
+            data: inputRef.current.value,
+        }))
+        inputRef.current.value = "";
+    }
+
     return (
-        <div className = "chat">
-            <div id = "idchat">
-            </div>
+        <div
+            onKeyDown={e => {if (e.key === 'Enter' && inputRef.current.value !== "") sendHandler()}}
+            className = "chat">
+            <div className = "msg_container" id = "idchat"></div>
             <div className = "send_container">
-                <input ref = {inputRef} type = 'text'></input>
+                <input placeholder = "Enter" className = "msg_input" ref = {inputRef} type = 'text'></input>
                 <button
-                    onClick={() => {
-                        socket.current.send(JSON.stringify({
-                            id: params.id,
-                            username: username,
-                            method: 'message',
-                            data: inputRef.current.value,
-                        }))
-                    }}
+                    onClick={() => {sendHandler()}}
                     className = "send_button">Send</button>
             </div>
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -6,3 +6,11 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
+// import React from 'react'
+// import { createRoot } from 'react-dom/client';
+// import App from "./App";
+//
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<App />);
