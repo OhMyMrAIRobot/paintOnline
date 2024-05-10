5cd9d6c1e21925a3fa3e6398ce3c189c170d224f
MrAIRobot <gfhieemobilelegends@gmail.com>
2024-05-10 19:28:36 +0300

    save canvas to database

--- client/src/Handlers/InitialiseCanvasHandler.js
+++ client/src/Handlers/InitialiseCanvasHandler.js
@@ -1,12 +1,12 @@
 import canvasState from "../Store/CanvasState";
 import axios from "axios";
-import {PushUndoHandler} from "./PushUndoHandler";
 import {LoadCanvas} from "./LoadCanvas";
 
 export const InitialiseCanvas = () => {
     return axios.get(`http://localhost:3000/initialise?id=${canvasState.session}`)
         .then(response => {
-            const saveHTML = response.data.canvas;
+            console.log(response.data.Canvas)
+            const saveHTML = response.data.Canvas;
 
             if (saveHTML) {
                 LoadCanvas(saveHTML)
